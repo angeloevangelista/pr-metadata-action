6302d476c33e58bee59d1e4b70395c2338eea92e
Angelo Evangelista <angeloevan.ane@gmail.com>
2022-08-30 16:09:00 -0300

    Initial action structure

---
+++ index.js
@@ -0,0 +1,104 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+const main = async () => {
+  try {
+    /**
+     * Getting all provided variables we want to use in our program
+     **/
+    const owner = core.getInput('owner', { required: true });
+    const repo = core.getInput('repo', { required: true });
+    const pr_number = core.getInput('pr_number', { required: true });
+    const token = core.getInput('token', { required: true });
+
+    /**
+     * Creating instance of Octokit. This will let us to call the 
+     * GitHub's REST API endpoints
+     * 
+     * You can find all the information about how to use Octokit here:
+     * https://octokit.github.io/rest.js/v18
+     **/
+    const octokit = new github.getOctokit(token);
+
+    /**
+     * We need to fetch the list of files that were changes in the Pull Request
+     * and store them in a variable.
+     * We use octokit.paginate() to automatically loop over all the pages of the
+     * results.
+     * Reference: https://octokit.github.io/rest.js/v18#pulls-list-files
+     */
+    const { data: changedFiles } = await octokit.rest.pulls.listFiles({
+      owner,
+      repo,
+      pull_number: pr_number,
+    });
+
+    /**
+     * Contains the sum of all the additions, deletions, and changes
+     * in all the files in the Pull Request.
+     **/
+    let diffData = {
+      additions: 0,
+      deletions: 0,
+      changes: 0
+    };
+
+    // Reference for how to use Array.reduce():
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce
+    diffData = changedFiles.reduce((acc, file) => {
+      acc.additions += file.additions;
+      acc.deletions += file.deletions;
+      acc.changes += file.changes;
+      return acc;
+    }, diffData);
+
+    /**
+     * Loop over all the files changed in the PR and add labels according 
+     * to files types.
+     **/
+    for (const file of changedFiles) {
+      /**
+       * Add labels according to file types.
+       */
+      const fileExtension = file.filename.split('.').pop();
+
+      const fileTypeLabelMap = {
+        md: 'markdown',
+        js: 'javascript',
+        yml: 'yaml',
+        yaml: 'yaml',
+      }
+
+      const tagToAdd = fileTypeLabelMap[fileExtension];
+
+      if (tagToAdd)
+        await octokit.rest.issues.addLabels({
+          owner,
+          repo,
+          issue_number: pr_number,
+          labels: [tagToAdd],
+        });
+    }
+
+    /**
+     * Create a comment on the PR with the information we compiled from the
+     * list of changed files.
+     */
+    await octokit.rest.issues.createComment({
+      owner,
+      repo,
+      issue_number: pr_number,
+      body: `
+        Pull Request #${pr_number} has been updated with: \n
+        - ${diffData.changes} changes \n
+        - ${diffData.additions} additions \n
+        - ${diffData.deletions} deletions \n
+      `
+    });
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+// Call the main function to run the action
+main();
